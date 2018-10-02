92383e7ad2293d3b625c732f7912e7a1568658fe
annettethomas <42778997+annettethomas@users.noreply.github.com>
2018-10-02 13:49:37 -0600

    exercise5

---
+++ Exercise5/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+
+        <header-shared></header-shared>
+
+        <hr>
+        <div class="row">
+
+            <server-comp></server-comp>
+
+            <serverde-comp></serverde-comp>
+        </div>
+        <hr>
+
+        <footer-shared></footer-shared>
+    </div>
+</template>
+
+<script>
+    import Header from './shared/Header.vue'
+    import Footer from './shared/Footer.vue'
+    import Server from './components/Server.vue'
+    import ServerDetails from './components/ServerDetails.vue'
+
+    export default {
+        components: {
+            'header-shared': Header,
+            'footer-shared': Footer,
+            'server-comp': Server,
+            'serverde-comp': ServerDetails
+        }
+    }
+</script>
+
+<style>
+
+</style>
